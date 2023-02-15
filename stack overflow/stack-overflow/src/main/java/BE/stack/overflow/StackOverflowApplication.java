538e57ab7dfcba13bb192d24f08728d95bc60a26
Dvdhan <han2041126@gmail.com>
2023-02-15 16:17:44 +0900

    111

---
+++ stack overflow/stack-overflow/src/main/java/BE/stack/overflow/StackOverflowApplication.java
@@ -0,0 +1,13 @@
+package BE.stack.overflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverflowApplication.class, args);
+	}
+
+}
