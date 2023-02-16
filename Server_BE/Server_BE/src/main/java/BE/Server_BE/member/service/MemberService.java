63277f935d3380376614269f02e4b30c24aabb62
Dvdhan <han2041126@gmail.com>
2023-02-16 16:34:14 +0900

    [fetch] 예외처리클래스 수정

--- Server_BE/Server_BE/src/main/java/BE/Server_BE/member/service/MemberService.java
+++ Server_BE/Server_BE/src/main/java/BE/Server_BE/member/service/MemberService.java
@@ -18,34 +18,34 @@ public class MemberService {
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
     public Member createMember(Member member) throws Exception {
         verifyEmail(member);
+        // 등록된 이메일이 있을 경우, 예외 처리.
         return memberRepository.save(member);
     }
     // 중복 이메일이면 이메일 중복 예외 처리하기
     public Member updateMember(Member member) throws Exception{
         Member findMember = loadMember(member.getMemberId());
-        if (findEmail(member) == loadMember(member.getMemberId()))
-
+        // 입력된 id로 불러올 회원정보가 없다면 예외처리
+        verifyEmail(member);
+        // 입력된 회원정보에 이메일이 중복이라면 이메일 중복 예외 처리.
         Optional.ofNullable(member.getNickName())
                         .ifPresent(nickname -> findMember.setNickName(nickname));
         Optional.ofNullable(member.getPassword())
                         .ifPresent(password -> findMember.setPassword(password));
         Optional.ofNullable(member.getAbout_Me())
                         .ifPresent(aboutMe->findMember.setAbout_Me(aboutMe));
-
         return memberRepository.save(findMember);
     }
     // 회원 아이디로 조회되는게 없으면 회원이 없다고 예외처리하기
     public Member getMember(long memberId) {
         return loadMember(memberId);
     }
-    //데이터가 없을 경우, 저장된 데이터가 없다고 예외처리하기
-    public Page<Member> getMembers(int page, int size) {
+
+    public Page<Member> getMembers(int page, int size) throws Exception{
         if (memberRepository.findAll() == null) {
             throw new BusinessLogicException(ExceptionCode.DATA_IS_EMPTY);
-        }else
+        }
         return memberRepository.findAll(PageRequest.of(
                 page, size
         ));
@@ -54,15 +54,11 @@ public class MemberService {
         Member deleteMember = loadMember(memberId);
         memberRepository.delete(deleteMember);
     }
-
     public void verifyEmail(Member member) throws Exception {
         Member findMember = memberRepository.findByEmail(member.getEmail());
         if (findMember != null) {
-            throw new BusinessLogicException(ExceptionCode.MEMBER_ALREADY_EXIST);
+            throw new BusinessLogicException(ExceptionCode.EMAIL_IS_DUPLICATED);
         }
-    }
-    public Member findEmail (Member member) {
-        return memberRepository.findByEmail(member.getEmail());
     }
     public Member loadMember(long memberId) {
         Member findMember = memberRepository.findById(memberId);
