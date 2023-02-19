b356055bb9c1f28b2a15ef4246d53d2c1dddb3e3
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-19 21:57:21 +0900

    [feat] Header 컴포넌트 작성

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,55 @@
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { GrSearch } from 'react-icons/gr';
+import { Link, useNavigate } from 'react-router-dom';
+import { LinkButton, BlueLinkButton } from './Buttons';
+import { StyledHeader, StyledWrapper } from './styled/Header.styled';
+import { useState } from 'react';
+
 const Header = () => {
-  return <div>Header</div>;
+  const [str, setStr] = useState('');
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setStr(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setStr('');
+    navigate('/', { replace: false });
+  };
+
+  return (
+    <StyledHeader>
+      <StyledWrapper>
+        <Link className="menu" to="#">
+          <RxHamburgerMenu />
+        </Link>
+        <Link id="logo" to="/">
+          <span>Stack Overflow</span>
+        </Link>
+        <form onSubmit={onSubmit}>
+          <div>
+            <GrSearch />
+            <input
+              onChange={onChange}
+              value={str}
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+        </form>
+        <div>
+          <LinkButton to={'/login'} width="60px">
+            Log in
+          </LinkButton>
+          <BlueLinkButton to={'/signup'} width="66px">
+            Sign up
+          </BlueLinkButton>
+        </div>
+      </StyledWrapper>
+    </StyledHeader>
+  );
 };
 
 export default Header;
