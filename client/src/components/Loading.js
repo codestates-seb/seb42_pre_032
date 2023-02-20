6f2191d0ff0c179a84faddd2f93d3f3de8361eff
yeojuyeong <yjy8606@gmail.com>
2023-02-20 10:56:58 +0900

    사이드바 로딩 구현

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,5 +1,38 @@
+import styled from 'styled-components';
+import React, { useState, useEffect } from 'react'; // eslint-disable-line no-unused-vars
+
+const LoadingCom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  height: calc(100vh - 50px);
+`;
+
 const Loading = () => {
-  return <div>Loading</div>;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <div>
+      <LoadingCom>
+        {loading ? (
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"
+            alt="loading icon"
+            style={{ maxWidth: '100%' }}
+          />
+        ) : (
+          <p></p>
+        )}
+      </LoadingCom>
+    </div>
+  );
 };
 
 export default Loading;
