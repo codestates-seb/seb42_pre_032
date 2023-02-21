6f0704f80582f305b2feebef6671211d2fd04c69
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-21 14:30:47 +0900

    [feat] update, create 페이지 추가

--- client/src/page/UpdatePage.js
+++ client/src/page/UpdatePage.js
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+// import { useParams } from 'react-router-dom';
+import Editor from '../components/Editor';
+
 const UpdatePage = () => {
-  return <div>UpdatePage</div>;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  // const { id } = useParams();
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <>
+      <Editor
+        title={title}
+        content={content}
+        setContent={setContent}
+        setTitle={onTitleChange}
+      />
+    </>
+  );
 };
 
 export default UpdatePage;
