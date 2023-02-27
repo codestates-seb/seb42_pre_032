0212416d12d01537ef314ddc7519e50061eb22ff
jinaSE0 <tiffany6771@gmail.com>
2023-02-28 01:23:08 +0900

    동적라우팅 구현

--- client/src/page/MainPage.js
+++ client/src/page/MainPage.js
@@ -1,6 +1,7 @@
 import Main from '../components/Main';
 import styled from 'styled-components';
 import Sidebar from '../components/Sidebar';
+import { useEffect, useState } from 'react';
 
 const MainPageContainer = styled.div`
   display: flex;
@@ -9,10 +10,34 @@ const MainPageContainer = styled.div`
 `;
 
 const MainPage = () => {
+  //데이터 저장하는 함수
+  const [data, setData] = useState([]);
+
+  // console.log(data);
+
+  useEffect(() => {
+    async function fetchdata() {
+      const response = await fetch('/boards', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzM4NCJ9.eyJyb2xlcyI6WyJBRE1JTiIsIlVTRVIiXSwidXNlcm5hbWUiOiJhZG1pbkBnbWFpbC5jb20iLCJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE2Nzc0OTgyNjIsImV4cCI6MTY3NzU4NDY2Mn0.DX5IPNVkG2yQelMoRHtjQbXV3z6dkMo77Aob-chnrhugrD681D3HzeaVJQGeC-aL',
+        },
+        //body: JSON.stringify({}),
+        credentials: 'include',
+      });
+      const data = await response.json();
+      // console.log(data.data[0].title);
+      setData(data.data);
+    }
+    fetchdata();
+  }, []);
+
   return (
     <MainPageContainer>
       <Sidebar></Sidebar>
-      <Main></Main>
+      <Main data={data}></Main>
     </MainPageContainer>
   );
 };
